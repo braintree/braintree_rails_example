c99198b4a28383494e2a1d8d1dc1b15a4c5cdd48
pblesi <code@getbraintree.com>
2015-09-02 21:27:59 +0000

    Add route for GET /checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'checkout' => 'checkout#index'
   get 'checkout/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
