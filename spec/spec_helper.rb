a07021e055210a8d2661c012591f959b042e638e
pblesi <code@getbraintree.com>
2015-10-07 19:26:15 +0000

    Prevent Braintree from logging during tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,11 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.before(:suite) do
+    # Prevent Braintree from logging to standard out during tests
+    Braintree::Configuration.logger = Logger.new("/dev/null")
+  end
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
