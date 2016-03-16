cd810b6a2c6c258e992e2fea5b62608e33c21652
blade <code@getbraintree.com>
2016-03-16 18:39:57 +0000

    Display transaction success or failure message conditionally

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -4,6 +4,7 @@ class CheckoutsController < ApplicationController
   end
 
   def show
+    @result = flash[:transaction_result]
     @transaction = Braintree::Transaction.find(params[:id])
   end
 
@@ -17,9 +18,18 @@ class CheckoutsController < ApplicationController
     )
 
     if result.success?
+      flash[:transaction_result] = {
+        :header => "Sweet Success!",
+        :icon => "success",
+        :result => "Your test transaction has been successfully processed. See the Braintree API response and try again."
+      }
       redirect_to checkout_path(result.transaction.id)
     elsif result.transaction
-      flash[:error] = "Transaction status - #{result.transaction.status}"
+      flash[:transaction_result] = {
+        :header => "Transaction Failed",
+        :icon => "fail",
+        :result => "Your test transaction has a status of #{result.transaction.status}. See the Braintree API response and try again."
+      }
       redirect_to checkout_path(result.transaction.id)
     else
       error_messages = result.errors.map { |error| "Error: #{error.code}: #{error.message}" }
