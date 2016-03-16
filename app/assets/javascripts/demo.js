dea0dbf123dfdc598efc693f27ce43447e670d2c
blade <code@getbraintree.com>
2016-03-16 18:27:10 +0000

    Update javascript assets

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -2,16 +2,9 @@ function Demo(config){
   this.config = config;
   this.config.development = config.development || false;
 
-  this.bt = $('.braintree');
   this.paymentForm = $('#' + config.formID);
   this.inputs = $('input[type=text], input[type=email], input[type=tel]');
   this.button = this.paymentForm.find('.button');
-  this.checkout = $('.checkout');
-  this.success = $('.success');
-  this.hashLinks = $('a[href^=#]');
-  this.successView = window.location.origin + '/' + this.paymentForm.attr('action');
-
-  console.log(this.hashLinks);
 
   this.states = {
     'show' : 'active',
@@ -19,7 +12,6 @@ function Demo(config){
   };
   this.focusClass = "has-focus";
   this.valueClass = "has-value";
-  this.transitionend = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
 
   this.initialize();
 }
@@ -32,20 +24,12 @@ Demo.prototype.initialize = function(){
   this.inputs.each(function(index, element){
     self.labelHander($(element));
   });
+  this.notify('error');
 };
 
 
 Demo.prototype.events = function(){
   var self = this;
-
-  this.paymentForm.on('submit', function(event){
-    self.process(event);
-  });
-
-  this.hashLinks.on('click', function(event){
-    console.log('hashLinks element clicked');
-    self.resetCheckout(event);
-  });
 
   this.inputs.on('focus', function(){
       $(this).closest('label').addClass(self.focusClass);
@@ -55,20 +39,6 @@ Demo.prototype.events = function(){
     }).on('blur', function(){
       $(this).closest('label').removeClass(self.focusClass);
       self.labelHander($(this));
-  });
-
-  $(document).on( 'payment', function(event, status){
-    if (status === 'success') {
-      self.bt.addClass('active');
-      self.confirmation();
-    } else {
-      self.bt.removeClass('active');
-      self.notify(status);
-    }
-  });
-
-  $(document).keyup(function(event) {
-    if (event.keyCode === 27) self.resetCheckout(event);
   });
 };
 
@@ -93,7 +63,7 @@ Demo.prototype.labelHander = function(element){
 Demo.prototype.notify = function(status){
   var self = this;
   var notice = $('.notice-' + status );
-  var delay = (this.config.development === true) ? 2000 : 0;
+  var delay = (this.config.development === true) ? 4000 : 2000;
 
   notice.show()
 
@@ -108,67 +78,4 @@ Demo.prototype.notify = function(status){
       }, 310);
     }, delay);
   }, 10);
-};
-
-
-Demo.prototype.process = function(event){
-  var self = this;
-  var json = this.getFormJSON(self.paymentForm);
-  var callback = this.config.onPurchase;
-  var delay = (this.config.development === true) ? 1000 : 0;
-
-  event.preventDefault();
-  self.bt.addClass('active');
-  self.button.addClass(self.states.wait);
-
-  console.log(self.successView);
-
-  window.setTimeout(function(){
-    callback(json);
-  }, delay);
-};
-
-
-Demo.prototype.confirmation = function(){
-  var self = this;
-
-  this.button.addClass('out').removeClass('pulse');
-  this.checkout.removeClass(this.states.show).addClass('dismiss');
-  this.success.addClass(this.states.show);
-
-  window.setTimeout(function(){
-    self.button.removeClass(self.states.wait);
-
-    // window.location.pathname = 'success.html';
-
-    window.location = self.successView;
-  }, 500);
-};
-
-
-Demo.prototype.resetCheckout = function(event){
-  event.preventDefault();
-
-  this.button.removeClass('out').addClass('pulse');
-  this.success.removeClass(this.states.show);
-  this.checkout.addClass(this.states.show);
-};
-
-
-Demo.prototype.getFormJSON = function(form){
-  var data = {};
-  var fields = form.serializeArray();
-
-  $.each(fields, function() {
-    if (data[this.name] !== undefined) {
-      if (!data[this.name].push) {
-        data[this.name] = [data[this.name]];
-      }
-      data[this.name].push(this.value || '');
-    } else {
-      data[this.name] = this.value || '';
-    }
-  });
-
-  return data;
 };
