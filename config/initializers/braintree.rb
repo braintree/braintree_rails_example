15079b4ce42eaadecca7d4b88e2517bcd37ad53f
Braintree <code@getbraintree.com>
2019-08-30 17:41:26 +0000

    Throw error when missing env variables

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,1 +1,5 @@
 Dotenv.load
+
+if !ENV["BT_ENVIRONMENT"] || !ENV["BT_MERCHANT_ID"] || !ENV["BT_PUBLIC_KEY"] || !ENV["BT_PRIVATE_KEY"]
+  raise "Cannot find necessary environmental variables. See https://github.com/braintree/braintree_rails_example#setup-instructions for instructions";
+end
